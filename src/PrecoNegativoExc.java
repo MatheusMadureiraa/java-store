334caab71f50240ab7a96fcaa342170d76d4dc9a
Matheus Madureira <143838250+MatheusMadureiraa@users.noreply.github.com>
2025-07-30 14:54:55 -0300

    Delete src directory

--- src/PrecoNegativoExc.java
+++
@@ -1,8 +0,0 @@
-public class PrecoNegativoExc extends Exception{
-    
-    Leitura l = new Leitura();
-
-    public void printPrecoNegativo(){
-        System.out.println("\nO preco do produto deve ser um numero positivo!\n");
-    }
-}
